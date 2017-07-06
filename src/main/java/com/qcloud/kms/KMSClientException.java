8cc5cdfaeda1485b7f945a819337ac088282d860
wuxiaocong <wuxc@gionee.com>
2017-07-06 09:56:39 +0800

    格式化并加入变量 private static final long serialVersionUID 以解决警告

--- src/main/java/com/qcloud/kms/KMSClientException.java
+++ src/main/java/com/qcloud/kms/KMSClientException.java
@@ -1,8 +1,10 @@
 package com.qcloud.kms;
 
+public class KMSClientException extends RuntimeException {
+    
+    private static final long serialVersionUID = 7464814184215908274L;
 
-public class KMSClientException extends RuntimeException{
-	public KMSClientException(String message) {
+    public KMSClientException(String message) {
         super(message);
     }
 }
